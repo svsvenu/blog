423eba0dac1c996fb2927bbd1839cd5d78ce862d
venusurampudi <venusurampudi@venus-mbp.home>
2016-01-02 10:24:26 -0600

    c

---
+++ camel-fsw/camel-fsw-ear/src/main/java/org/camel/fsw/ear/App.java
@@ -0,0 +1,13 @@
+package org.camel.fsw.ear;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
